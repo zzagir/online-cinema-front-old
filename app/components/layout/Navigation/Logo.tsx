eef4d8dbf9aefdf6396c6dcb1236670f83dc81a7
zzagir <zaynalov005@mail.ru>
2023-08-09 00:15:32 +0300

    6 urok

---
+++ app/components/layout/Navigation/Logo.tsx
@@ -0,0 +1,22 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import { FC } from 'react'
+
+import logoImage from '@/assets/images/logo.svg'
+
+const Logo: FC = () => {
+	return (
+		<Link href="/">
+			<a className="px-layout mb-10 block">
+				<Image
+					src={logoImage}
+					width={247}
+					height={34}
+					alt="Online cinema logo"
+					draggable={false}
+				/>
+			</a>
+		</Link>
+	)
+}
+export default Logo
